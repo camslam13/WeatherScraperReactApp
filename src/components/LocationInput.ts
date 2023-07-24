865985d37d4ebf0669ed21061f57be30060ef9b6
camslam13 <114112615+camslam13@users.noreply.github.com>
2023-07-23 22:59:28 -0400

    Update LocationInput.ts

--- src/components/LocationInput.ts
+++ src/components/LocationInput.ts
@@ -1,1 +1,32 @@
+import { useState } from 'react';
 
+const LocationInput: React.FC<{ onLocationSubmit: (latitude: number, longitude: number) => void }> = ({ onLocationSubmit }) => {
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  const handleSubmit = () => {
+    if (latitude && longitude) {
+      onLocationSubmit(parseFloat(latitude), parseFloat(longitude));
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={latitude}
+        onChange={(e) => setLatitude(e.target.value)}
+        placeholder="Enter latitude..."
+      />
+      <input
+        type="text"
+        value={longitude}
+        onChange={(e) => setLongitude(e.target.value)}
+        placeholder="Enter longitude..."
+      />
+      <button onClick={handleSubmit}>Get Weather Info</button>
+    </div>
+  );
+};
+
+export default LocationInput;
